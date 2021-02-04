242387c718ab95cd05ab81315b0aa230ac5bb4fb
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-02-04 13:33:22 +0200

    todo

---
+++ my-todo/src/components/Todo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Todo = () => {
+  return (
+    <div className="todo">
+      <li className="todo-item">Hello</li>
+      <button>
+        <i className="fas fa-check"></i>
+      </button>
+      <button>
+        <i className="fas fa check"></i>
+      </button>
+    </div>
+  );
+};
+export default Todo;
