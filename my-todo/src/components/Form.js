e1548fdc98a8e251ef5378c2fd4dcadc9b52b565
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-02-04 12:59:42 +0200

    updating-state

--- my-todo/src/components/Form.js
+++ my-todo/src/components/Form.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Form = () =>{
+const Form = ({setInputText}) =>{
     // we going to write javascript code and function
     const inputTextHandler = (e)=>{
         console.log(e.target.value);
+        setInputText(e.target.value);
     };
     return(
         <form>
